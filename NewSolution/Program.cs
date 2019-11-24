3c3ee11cb1acd4c9193686bfb3f0c7782ee4a023
elixa55 <fodoreditbp@gmail.com>
2019-11-24 21:42:49 +0100

    I've added a new line to Program.cs

--- NewSolution/Program.cs
+++ NewSolution/Program.cs
@@ -8,6 +8,8 @@ namespace NewSolution
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Na ezzel változott a code");
+            Console.WriteLine("Ez egy uj sor, ezt most commitolom");
+            
             Console.ReadKey(true);
         }
     }
