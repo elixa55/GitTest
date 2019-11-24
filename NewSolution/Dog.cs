bd4137ba04460a9cc1bb470dc374a44b42fce67b
elixa55 <fodoreditbp@gmail.com>
2019-11-24 22:03:17 +0100

    I've created a new class: Dog.cs

---
+++ NewSolution/Dog.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NewSolution
+{
+    class Dog
+    {
+        public void method()
+        {
+            Console.WriteLine("I'm a method of Dog class");
+        }
+    }
+}
