ac33efdcc474a2751d81c419cb070116b082412d
elixa55 <fodoreditbp@gmail.com>
2019-11-24 22:02:57 +0100

    This is a new class: Cat

---
+++ NewSolution/Cat.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace NewSolution
+{
+    class Cat
+    {
+        public void method()
+        {
+            Console.WriteLine("I'm a method of Cat class");
+            Console.WriteLine("This is the end");
+        }
+    }
+}
